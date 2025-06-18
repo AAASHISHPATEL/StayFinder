e105faaa7abd6cd8aff8da147c49cfb9f20ff4eb
ASHISH PATEL <pashishp005@gmail.com>
2025-06-18 17:09:51 +0530

    token fix

--- backend/middleware/isAuth.js
+++ backend/middleware/isAuth.js
@@ -2,7 +2,7 @@ import jwt from "jsonwebtoken"
 const isAuth = async (req,res,next) => {
 
     try {
-        let {token} = req.cookies
+        const token = req.cookies.token || req.headers.authorization?.split(" ")[1]
         if(!token){
             res.status(400).json({message:"user doesn't have a token"})
         }
