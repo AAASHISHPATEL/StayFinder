67f8c4eb419b2fbe8c25183b0a3dad19af49f10f
ASHISH PATEL <pashishp005@gmail.com>
2025-06-22 14:51:36 +0530

    Some functionality chnaged

--- backend/controllers/auth.controller.js
+++ backend/controllers/auth.controller.js
@@ -18,7 +18,7 @@ export const sighUp=async (req,res) => {
             secure: true, 
             sameSite: "None",
             maxAge: 7 * 24 * 60 * 60 * 1000,
-            path: "/"
+            path: "/"   
 
 
         })
